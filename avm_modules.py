929d0fddddaecd7cc15534149f78fa28725dd65a
jeffrey82221 <jeffrey82221@gmail.com>
2021-06-02 10:24:30 +0800

    modify lightning module

--- avm_modules.py
+++ avm_modules.py
@@ -3,61 +3,189 @@ from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import add_self_loops, degree
 import torch.nn.functional as F
 
+
 import pytorch_lightning as pl 
 from torch_geometric.data import DataLoader 
+
+from pl_bolts.optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
 from avm_etl_module import get_geometric_data
 
+from torchmetrics import MeanSquaredError
+from torchmetrics import MeanAbsoluteError
+
+test_mse = MeanSquaredError(compute_on_step=False)
+test_mae = MeanAbsoluteError(compute_on_step=False)
+
+valid_mse = MeanSquaredError(compute_on_step=False)
+valid_mae = MeanAbsoluteError(compute_on_step=False)
+
 class AVMProjModule(pl.LightningModule):
-    def __init__(self, num_node_features = 2, output_dim=1, hidden_dim = 5, batch_size = 1, lr = 0.001):
+    def __init__(self, hidden_dim = 5, batch_size = 1, lr = 0.001):
         super(AVMProjModule, self).__init__()
-        self.model = GraphNeuralNet(num_node_features, hidden_dim = hidden_dim, output_dim = output_dim)
+        
+        # Step 1: 載入模型參數
+        self.hidden_dim = hidden_dim
+        
+        # Step 2: 載入data-dependent參數
+        self.num_node_features = 2 
+        self.output_dim = 1 
+        
+        # Step 3: 載入訓練參數
         self.batch_size = batch_size
         self.lr = lr 
+        
+        # Step 4: 建立模型 
+        self.model = GNN(self.num_node_features, hidden_dim = self.hidden_dim, output_dim = self.output_dim)
+        
+        self._batch_cnt = 0
+        self._architecture_logged = False
+        self._best_val_loss = 1e13
+        self._best_val_mae = 1e13
         
     def forward(self, x, edge_index):
         y = self.model(x, edge_index)
         return y 
     
+    def on_train_start(self):
+        self._batch_cnt = 0
+        self._architecture_logged = False
+        self._best_val_loss = 1e13
+        self._best_val_mae = 1e13
+        self.log('best_val_loss', self._best_val_loss)
+        self.log('best_val_mae', self._best_val_mae)
+    
+    def on_fit_start(self): 
+        self._batch_cnt = 0
+        self.logger.log_hyperparams(
+            params = {
+                "hidden_dim": self.hidden_dim,
+                "num_node_features": self.num_node_features, 
+                "output_dim": self.output_dim,
+                "batch_size": self.batch_size, 
+                "lr": self.lr
+            }, 
+            metrics = {
+                "best_val_loss": self._best_val_loss
+            }
+        )
+    
     def training_step(self, batch, batch_idx):
+        
+        # Step 1: calculate training loss 
         x, y, edge_index = batch.x, batch.y, batch.edge_index 
         y_hat = self(x, edge_index)
         loss = F.l1_loss(y_hat, y)
-        self.log('avm/train_loss',loss)
+        
+        # Step 2: log values 
+        self.logger.experiment.add_scalar(f'Train/loss', loss, self._batch_cnt)
+        
+        # Step 3: log neural architecture 
+        if not self._architecture_logged: 
+            self.logger.experiment.add_graph(self, [x, edge_index])
+            print("Model Architecture Logged")
+            self._architecture_logged = True
+        
+        # Step 4: increase batch count 
+        self._batch_cnt += 1 
         return loss
     
     def validation_step(self, batch, batch_idx):
+        # Step 1: calculate validation loss 
         x, y, edge_index = batch.x, batch.y, batch.edge_index 
         y_hat = self(x, edge_index)
         loss = F.l1_loss(y_hat, y)
+        
+        valid_mse(y_hat, y)
+        valid_mae(y_hat, y)
+        
+        # Step 2: log value
+        self.log('val_loss', loss)
         return {'val_loss': loss}
     
     def validation_epoch_end(self, outputs):
-        avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
-        self.log('avm/val_loss', avg_loss)
+        if self.current_epoch > 0:
+            # Step 1: calculate average loss and metric values 
+            avg_loss = torch.stack([x['val_loss'] for x in outputs]).mean()
+            
+            mse = valid_mse.compute()
+            mae = valid_mae.compute()
+            
+            # Step 2: log total loss  
+            self.logger.experiment.add_scalar("Val/total_loss", avg_loss, self.current_epoch)
+            self.logger.experiment.add_scalar("Val/mse", mse, self.current_epoch)
+            self.logger.experiment.add_scalar("Val/mae", mae, self.current_epoch)
+            
+            # Step 3: replace and log best values
+            if avg_loss < self._best_val_loss:
+                self._best_val_loss = avg_loss 
+                # loss the best_val_loss 
+                self.log('best_val_loss', self._best_val_loss)
+            if mae < self._best_val_mae:
+                self._best_val_mae = mae 
+                # loss the best_val_loss 
+                self.log('best_val_mae', self._best_val_mae)
+                
+    def test_step(self, batch, batch_idx):
+        # Step 1: calculate output 
+        x, y, edge_index = batch.x, batch.y, batch.edge_index 
+        y_hat = self(x, edge_index)
         
+        # Step 2: apply metrics 
+        test_mse(y_hat, y)
+        test_mae(y_hat, y)
+        return {'result': 'test'}
+    def on_test_epoch_end(self):
+        mse = test_mse.compute().item()
+        mae = test_mae.compute().item()
+        result = {
+            'mse': mse, 
+            'mae': mae
+            }
+        print(result)
+        return result
+    
     def prepare_data(self):
         self.geometric_data = get_geometric_data()
     
     def train_dataloader(self):
-        return DataLoader([self.geometric_data], batch_size=self.batch_size)
+        return DataLoader(
+            [self.geometric_data], 
+            shuffle=True, 
+            batch_size=self.batch_size, 
+            num_workers=4)
     
     def val_dataloader(self):
-        return DataLoader([self.geometric_data], batch_size=self.batch_size)
+        return DataLoader(
+            [self.geometric_data], 
+            batch_size=self.batch_size, 
+            num_workers=4,
+            pin_memory=True
+        )
+    
+    def test_dataloader(self):
+        return DataLoader(
+            [self.geometric_data], 
+            batch_size=self.batch_size, 
+            num_workers=4,
+            pin_memory=True
+        )
     
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=5e-4) 
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=5e-4)
+        lr_scheduler = LinearWarmupCosineAnnealingLR(optimizer, warmup_epochs=5, max_epochs=40)
+        return {
+            "optimizer": optimizer, 
+            "lr_scheduler": lr_scheduler
+        }
 
-
-class GraphNeuralNet(torch.nn.Module):
+class GNN(torch.nn.Module):
     def __init__(self, num_node_features, hidden_dim=2, output_dim=1):
-        super(GraphNeuralNet, self).__init__()
+        super(GNN, self).__init__()
         self.conv1 = GCNConv(num_node_features, hidden_dim)
         self.conv2 = GCNConv(hidden_dim, hidden_dim)
         self.conv3 = GCNConv(hidden_dim, output_dim)
-
     def forward(self, x, edge_index):
         # x, edge_index = data.x, data.edge_index
-
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
@@ -65,7 +193,6 @@ class GraphNeuralNet(torch.nn.Module):
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
         x = self.conv3(x, edge_index)
-
         return x
     
 class GCNConv(MessagePassing):
--- train_avm_module.py
+++ train_avm_module.py
@@ -1,18 +1,66 @@
 from avm_modules import AVMProjModule
+
 import pytorch_lightning as pl 
+from pytorch_lightning.utilities.seed import seed_everything
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor, ModelCheckpoint
 
+seed_everything(1, workers=True) # seed_everything，讓每次模型訓練都會產生一樣的結果，增進reproducability。
 
-project_module = AVMProjModule(hidden_dim=5)
+# 建立pytorch lightning模組 
+project_module = AVMProjModule(hidden_dim=5, batch_size = 1)
 
-# project_module = AVMProjModule.load_from_checkpoint('lightning_logs/version_34/checkpoints/epoch=12964-step=12964.ckpt')
-
-trainer = pl.Trainer(
-    # auto_scale_batch_size='power', 
-    auto_lr_find=True, 
-    progress_bar_refresh_rate=10000, 
-    min_epochs=100000000, 
-    max_epochs=10000000000
+# 建立Tensorboard logger 
+logger = TensorBoardLogger('/home/ai/work/logs/tensorboard', 
+                           # 儲存tensorboard log的根目錄
+                           'multi-task', 
+                           # 儲存此專案log的目錄 
+                           default_hp_metric=False, 
+                           # 若於lightning module中，有手動進行hyperparameter的logging的話，此處設為False 
+                           log_graph=True) 
+                           # 是否要於tensorboard查看模型架構 
+    
+# 在訓練過程中，把最好的模型參數存成checkpoint，以幫助事後快速載入
+checkpoint = ModelCheckpoint(
+    monitor='best_val_mae',
+    mode='min',
+    dirpath='./checkpoint',
+    filename='epoch{epoch:02d}-loss{best_val_mae:.2f}',
+    save_last=True,
+    auto_insert_metric_name=False,
+    every_n_val_epochs=1,
+    verbose=True
 )
 
+if __name__ == "__main__":
+    # 若有使用learning rate scheduler的話，可以用LearningRateMonitor把lr的變化log起來。
+    lr_monitor = LearningRateMonitor(logging_interval='epoch', log_momentum = True)
 
-trainer.fit(project_module)
+    # 加入early_stopping的機制
+    early_stopping = EarlyStopping('best_val_mae', mode='min', patience = 5, verbose=True)
+
+    trainer = pl.Trainer(
+        # auto_scale_batch_size='power', 
+        # 自動選擇最大的batch_size 
+        auto_lr_find=True, 
+        # 自動選擇最好的learning rate 
+        logger = logger, 
+        callbacks=[lr_monitor, early_stopping, checkpoint], 
+        deterministic=True, 
+        # 設定為deterministic，並且搭配前面的seed_everything，讓每次模型訓練都會產生一樣的結果，增進reproducability。
+        check_val_every_n_epoch=50,
+        progress_bar_refresh_rate=10000
+    )
+
+    trainer.tune(project_module)    
+    print('Hyper-parameters tuned')
+
+    trainer.fit(project_module)
+
+else:
+    project_module = AVMProjModule.load_from_checkpoint('./checkpoint/last.ckpt')
+    trainer = pl.Trainer(
+        deterministic=True
+    )
+    trainer.test(project_module)
+    
